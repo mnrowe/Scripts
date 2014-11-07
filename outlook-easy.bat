6fd299fba8ee4d74782b9287f9b98e80d2ee365b
00rwe <symbol@riseup.net>
2014-11-06 20:53:59 -0500

    removed some commas and fixed names

--- outlook-easy.bat
+++ outlook-easy.bat
@@ -28,27 +28,27 @@ cls
 set /p UserInputPath= user home folder: 
 
 if %whatapp%==1 (
-	Robocopy "C:\Users\%UserInputPath%\AppData\Roaming\microsoft\signatures" D:\userbackups\%UserInputPath%\backup\roaming\signatures *.* /e
-	Robocopy "C:\Users\%UserInputPath%\AppData\Local\Microsoft\Outlook" D:\userbackups\%UserInputPath%\backup\local\OUTLOOK *.pst /e
-	Robocopy "C:\Users\%UserInputPath%\AppData\Roaming\microsoft\outlook" D:\userbackups\%UserInputPath%\backup\roaming\autoc *.nk2 /e
+	Robocopy C:\Users\%UserInputPath%\AppData\Roaming\microsoft\signatures D:\userbackups\%UserInputPath%\backup\roaming\signatures *.* /e
+	Robocopy C:\Users\%UserInputPath%\AppData\Local\Microsoft\Outlook D:\userbackups\%UserInputPath%\backup\local\OUTLOOK *.pst /e
+	Robocopy C:\Users\%UserInputPath%\AppData\Roaming\microsoft\outlook D:\userbackups\%UserInputPath%\backup\roaming\autocomplete *.nk2 /e
 	echo Done
 	pause
 ) else if %whatapp%==2 (
-	Robocopy "D:\userbackups\%UserInputPath%\backup\roaming\signatures" C:\Users\%UserInputPath%\AppData\Roaming\microsoft\signatures *.* /e
-	Robocopy "D:\userbackups\%UserInputPath%\backup\local\OUTLOOK" C:\Users\%UserInputPath%\AppData\Local\Microsoft\Outlook *.pst /e
-	Robocopy "D:\userbackups\%UserInputPath%\backup\roaming\autoc" C:\Users\%UserInputPath%\AppData\Roaming\microsoft\outlook *.nk2 /e
+	Robocopy D:\userbackups\%UserInputPath%\backup\roaming\signatures C:\Users\%UserInputPath%\AppData\Roaming\microsoft\signatures *.* /e
+	Robocopy D:\userbackups\%UserInputPath%\backup\local\OUTLOOK C:\Users\%UserInputPath%\AppData\Local\Microsoft\Outlook *.pst /e
+	Robocopy D:\userbackups\%UserInputPath%\backup\roaming\autocomplete C:\Users\%UserInputPath%\AppData\Roaming\microsoft\outlook *.nk2 /e
 	echo Done
 	pause
 ) else if %whatapp%==3 (
 	Robocopy "C:\documents and settings\%UserInputPath%\application data\microsoft\signatures" D:\userbackups\%UserInputPath%\backup\roaming\signatures *.* /e
 	Robocopy "C:\documents and settings\%UserInputPath%\local settings\application data\microsoft\outlook" D:\userbackups\%UserInputPath%\backup\local\OUTLOOK *.pst /e
-	Robocopy "C:\documents and settings\%UserInputPath%\application data\microsoft\outlook" D:\userbackups\%UserInputPath%\backup\roaming\autoc *.nk2 /e
+	Robocopy "C:\documents and settings\%UserInputPath%\application data\microsoft\outlook" D:\userbackups\%UserInputPath%\backup\roaming\autocomplete *.nk2 /e
 	echo Done
 	pause
 ) else if %whatapp%==4 (
 	Robocopy D:\userbackups\%UserInputPath%\backup\roaming\signatures "C:\documents and settings\%UserInputPath%\application data\microsoft\signatures" *.* /e
 	Robocopy D:\userbackups\%UserInputPath%\backup\local\OUTLOOK "C:\documents and settings\%UserInputPath%\local settings\application data\microsoft\outlook" *.pst /e
-	Robocopy D:\userbackups\%UserInputPath%\backup\roaming\autoc "C:\documents and settings\%UserInputPath%\application data\microsoft\outlook" *.nk2 /e
+	Robocopy D:\userbackups\%UserInputPath%\backup\roaming\autocomplete "C:\documents and settings\%UserInputPath%\application data\microsoft\outlook" *.nk2 /e
 	echo Done
 	pause
 ) else (
